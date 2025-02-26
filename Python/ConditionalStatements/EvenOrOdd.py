3ddb396e601553c886c2431cfcf9efece37302c2
Krish Bharadwaj <167067349+KrishBharadwaj5678@users.noreply.github.com>
2025-02-26 20:18:55 +0530

    Add files via upload

---
+++ Python/ConditionalStatements/EvenOrOdd.py
@@ -0,0 +1,7 @@
+# Given a positive integer x. Your task is to check, if it is even or odd (Any number that gives zero as remainder when divided by 2 is an even number)
+
+a=int(input())
+if(a%2==0):
+  print("Even")
+else:
+  print("Odd")
