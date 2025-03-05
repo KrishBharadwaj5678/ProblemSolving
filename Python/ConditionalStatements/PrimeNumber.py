aaf01a97bbbd7c0de96e0d672dab7b798a5d821c
Krish Bharadwaj <167067349+KrishBharadwaj5678@users.noreply.github.com>
2025-03-05 07:32:51 +0530

    Add files via upload

---
+++ Python/ConditionalStatements/PrimeNumber.py
@@ -0,0 +1,16 @@
+"""
+Given a positive integer N, the task is to write a Python program to check if the number is Prime or not in Python.
+
+"""
+
+N=int(input("Enter a number: "))
+if(N==0 or N==1 or N<0):
+	print("Not Prime Number")
+else:
+	for i in range(2,N+1):
+		if(N==i):
+			print("Prime Number")
+		elif(N%i==0):
+			print("Not Prime Number")
+			break
+			
